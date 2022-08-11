bdc5c39ad5362376cc433b12054c3c8e6d6f2b41
Meg Porteous <meghan.porteous@gmail.com>
2022-08-11 22:27:56 +1000

    test int added for removal

--- GameState.java
+++ GameState.java
@@ -12,6 +12,7 @@ public class GameState {
     public static int fishTarget(){
 
         int min = 10;
+        int testInt = 0;
         int max = 20;
   
         int fishTarget = (int)Math.floor(Math.random()*(max-min+1)+min);
