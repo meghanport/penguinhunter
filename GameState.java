7c5d32e4e7943a096ab0b8c55ef9525808775be6
meghanport <meghan.porteous@gmail.com>
2022-08-12 10:14:23 +1000

    yoyoyoyoyo

--- GameState.java
+++ GameState.java
@@ -13,6 +13,7 @@ public class GameState {
 
         int min = 10;
         int testInt = 0;
+        int testtwo = 0;
         int max = 20;
   
         int fishTarget = (int)Math.floor(Math.random()*(max-min+1)+min);
